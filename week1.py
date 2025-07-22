305e8fe35c9174a6dc9358689db2a9c4068a3672
evos235 <161448262+evos235@users.noreply.github.com>
2025-07-22 15:27:15 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -55,3 +55,30 @@ output = nn.forward(X_input)
 
 print("\nOutput of the neural network for the input data:")
 print(output)
+
+
+
+
+Output:
+
+
+Initialized weights and biases:
+Weights (Input -> Hidden):
+ [[-1.14747139  0.71021461 -2.05642519 -2.30386875 -0.49380975]
+ [ 0.21580638  0.50791846 -1.46104385 -2.13937763  1.81400979]
+ [-0.62117137 -1.54467844 -1.18764075  0.09095533  0.37974042]]
+Biases (Hidden Layer):
+ [[0. 0. 0. 0. 0.]]
+Weights (Hidden -> Output):
+ [[-0.10784534 -0.0304473 ]
+ [-0.86899981  0.51846008]
+ [ 0.69159781  0.27519704]
+ [ 0.07207936 -0.18127564]
+ [ 0.13106008 -2.19868321]]
+Biases (Output Layer):
+ [[0. 0.]]
+
+Output of the neural network for the input data:
+[[0.48436552 0.17131137]
+ [0.44623155 0.16538175]]
+
